233f2bfe1ba9f5d1ddd000b5155ecb7a92ad27fa
dimsgusticode <dimsgusticode@gmail.com>
2023-10-02 21:56:45 +0800

    alert added

--- cityhub/app/components/navbar.js
+++ cityhub/app/components/navbar.js
@@ -6,6 +6,14 @@ const webName = 'Samarinda Hub';
 export default function Navbar(){
     return(
         <nav className="text-[#39393B] bg-[#F2F0E9] sticky top-0">
+            <div role="alert">
+                <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+                    Pemberitahuan
+                </div>
+                <div class="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+                    <p>Website masih dalam pengembangan!</p>
+                </div>
+            </div>
             <div className="flex justify-evenly p-8">
                 <div className="lg:text-lg">
                     <Link href='/' className={styles.btn}>{webName}</Link>
